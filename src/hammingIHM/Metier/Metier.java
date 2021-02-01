10084d998631a970cf651302793f9ffe49e8d7b7
Dimitri Dubois <dimitri.bleach@gmail.com>
2021-02-01 14:30:02 +0100

    isCodeCorrect dans Metier.java

--- src/hammingIHM/Metier/Metier.java
+++ src/hammingIHM/Metier/Metier.java
@@ -6,4 +6,22 @@ public class Metier
     {
 
     }
+
+    public boolean isCodeCorrect( String code, boolean presenterEtapes )
+    {
+        int nbBitsDebug = 0;
+
+        for (int i = 0; i < code.length(); i++)
+            if( Math.pow(2, i) > code.length() )
+            {
+                nbBitsDebug = i;
+                break;
+            }
+
+        for (int i = 0; i < nbBitsDebug; i++)
+            if( code.charAt((int) Math.pow(2, i)) != '0' )
+                return false;
+
+        return true;
+    }
 }
