ab71b5ad01a5a01952b5c6e3833cdb1aa828304f
Dimitri Dubois <dimitri.bleach@gmail.com>
2021-02-01 18:54:00 +0100

    merge et ajout de isCodeCorrect enfin

--- src/hammingIHM/metier/Metier.java
+++ src/hammingIHM/metier/Metier.java
@@ -1,159 +1,210 @@
 package hammingIHM.metier;
 
-import java.util.Arrays;
-
 public class Metier
 {
-	public Metier()
-	{
+    public Metier()
+    {
 
-	}
+    }
 
-	public String correctionPreEmission(String code)
-	{
-		String sRet;
+    public String correctionPreEmission(String code)
+    {
+        String sRet;
 
-		int nbBitsDebug = 0;
-		for (int i = 0; Math.pow(2, i) <= code.length(); i++)
-			nbBitsDebug++;
-		
-		//System.out.println(nbBitsDebug);
-		
-		//On allonge la chaine pour qu'elle fasse la taille de 2^nbBitsDebug-1-nbBitsDebug
-		if(code.length() != Math.pow(2, nbBitsDebug)- 1 - nbBitsDebug)
-			code = String.format("%" + (int)(Math.pow(2, nbBitsDebug) - 1 - nbBitsDebug) + "s", code);
-		
-		StringBuffer code2 = new StringBuffer(code);
-		
-		//on insere aux emplacement voulu des bits de hamming a la valeur _ car n'etant pas set
-		for (int i = 0; i < nbBitsDebug; i++) {
-			int bitHamming = code2.length() - (int)Math.pow(2, i) + 1;
-			code2.insert(bitHamming, '_');
-		}
-		
-		
-		//inversion du sens de la chaine pour etre naturel, on la reinversera a la fin
-		StringBuilder str = new StringBuilder(code2);
-		code2 = new StringBuffer(str.reverse().toString());
-		
-		//System.out.println(code2);
-		
-		sRet = nbBitsDebug +":";
-		
-		
-		//stockera chaque chaine lié a chaque bit de correction
-		String[] tabBits = new String[nbBitsDebug];
-		
-		
-		//Creations des chaines de bits, qui contiendront les bit determinant les bit correcteurs
-		//par exemple 7 5 3, 7 6 3, 7 6 5, ...
-		for (int i = 0; i < tabBits.length; i++)
-		{
-			String  bits = "";
-			int     j = i;
-			int     nbBitsAPrendre = (int)Math.pow(2, i);
-			int     nbBitsPris     = 0;
-			boolean toutPris       = false;
-			
-			//lectures des bits pour determiner les bit correcteurs via la methode Grey
-			for(int k=(int)(Math.pow(2, 0)-1); k<code2.length() && k< code2.length() - Math.pow(2, i); k++)
-			{
-				//System.out.println("\nk : " + k + "   " + nbBitsPris + "/" + nbBitsAPrendre + "  " + toutPris);
-				if(toutPris == true && nbBitsPris == 0)
-					toutPris = false;
-				else
-					if(toutPris == true && nbBitsPris > 0)
-					{
-						nbBitsPris--;
-						if(nbBitsPris == 0)
-							toutPris = false;
-					}
-				else
-					if(nbBitsPris < nbBitsAPrendre)
-					{
-						//System.out.print(code2.length()-1-k+1+" ");
-						bits += code2.charAt(code2.length()-1-k)+"";
-						nbBitsPris++;
-						if(nbBitsPris == nbBitsAPrendre)
-							toutPris = true;
-					}
-					
-			}
-			tabBits[i] = bits;
-			//System.out.println("\n" + tabBits[i] + "\n");
-		}
-		
-		//on lit les chaines et on addition chaque bit
-		int[] tabVal = new int[nbBitsDebug];
-		for (int i = tabBits.length-1; i >= 0; i--)
-		{
-			int val = 0;
-			for (int j = 0; j < tabBits[i].length(); j++)
-			{
-				if(val == 0 && tabBits[i].charAt(j) == '1')
-					val = 1;
-				else
-					if(val == 1 && tabBits[i].charAt(j) == '1')
-						val = 0;
-			}
-			tabVal[i] = val;
-			//System.out.println(val);
-		}
-		
-		//System.out.println(code2);
-		str = new StringBuilder(code2);
-		
-		//System.out.println(Arrays.toString(tabVal));
-		
-		
-		//placement des bits dans la chaine de retour
-		for (int i = 0; i < tabVal.length ; i++)
-		{
-			char ch = Character.forDigit(tabVal[i], 10);
-			//System.out.println(ch);
-			for (int j = 0; j < str.length(); j++)
-			{
-				if(str.charAt(j) == '_')
-				{
-					str.setCharAt(j, ch);
-					//System.out.println("C'est un " + str.charAt(i) + "au lieu d'un " + ch);
-					break;
-				}
-			}
-		}
-		
-		//on reinverse pour que le bit 1 soit a droite
-		code2 = new StringBuffer(str.reverse().toString());
-		//System.out.println(code2);
-		
-		sRet += code2.toString();
-		 
-		return sRet;
-	}
+        int nbBitsDebug = 0;
+        for (int i = 0; Math.pow(2, i) <= code.length(); i++)
+            nbBitsDebug++;
 
-	public boolean isCodeCorrect(String code, boolean presenterEtapes)
-	{
-		int nbBitsDebug = 0;
+        //System.out.println(nbBitsDebug);
 
-		for (int i = 0; i < code.length(); i++)
-			if (Math.pow(2, i) > code.length())
-			{
-				nbBitsDebug = i;
-				break;
-			}
+        //On allonge la chaine pour qu'elle fasse la taille de 2^nbBitsDebug-1-nbBitsDebug
+        if (code.length() != Math.pow(2, nbBitsDebug) - 1 - nbBitsDebug)
+            code = String.format("%" + (int) (Math.pow(2, nbBitsDebug) - 1 - nbBitsDebug) + "s", code);
 
-		for (int i = 0; i < nbBitsDebug; i++)
-			if (code.charAt((int) Math.pow(2, i)) != '0')
-				return false;
+        StringBuffer code2 = new StringBuffer(code);
 
-		return true;
-	}
-	
-	public static void main(String[] args)
-	{
-		Metier m = new Metier();
-		System.out.println(m.correctionPreEmission("1010"));
-		System.out.println(m.correctionPreEmission("1011"));
-		System.out.println(m.correctionPreEmission("10110111010"));
-	}
+        //on insere aux emplacement voulu des bits de hamming a la valeur _ car n'etant pas set
+        for (int i = 0; i < nbBitsDebug; i++)
+        {
+            int bitHamming = code2.length() - (int) Math.pow(2, i) + 1;
+            code2.insert(bitHamming, '_');
+        }
+
+
+        //inversion du sens de la chaine pour etre naturel, on la reinversera a la fin
+        StringBuilder str = new StringBuilder(code2);
+        code2 = new StringBuffer(str.reverse().toString());
+
+        //System.out.println(code2);
+
+        sRet = nbBitsDebug + ":";
+
+
+        //stockera chaque chaine lié a chaque bit de correction
+        String[] tabBits = new String[nbBitsDebug];
+
+
+        //Creations des chaines de bits, qui contiendront les bit determinant les bit correcteurs
+        //par exemple 7 5 3, 7 6 3, 7 6 5, ...
+        for (int i = 0; i < tabBits.length; i++)
+        {
+            StringBuilder bits = new StringBuilder();
+            int nbBitsAPrendre = (int) Math.pow(2, i);
+            int nbBitsPris = 0;
+            boolean toutPris = false;
+
+            //lectures des bits pour determiner les bit correcteurs via la methode Grey
+            for (int k = (int) (Math.pow(2, 0) - 1); k < code2.length() && k < code2.length() - Math.pow(2, i); k++)
+            {
+                //System.out.println("\nk : " + k + "   " + nbBitsPris + "/" + nbBitsAPrendre + "  " + toutPris);
+                if (toutPris && nbBitsPris == 0) toutPris = false;
+                else if (toutPris && nbBitsPris > 0)
+                {
+                    nbBitsPris--;
+                    if (nbBitsPris == 0) toutPris = false;
+                }
+                else if (nbBitsPris < nbBitsAPrendre)
+                {
+                    //System.out.print(code2.length()-1-k+1+" ");
+                    bits.append(code2.charAt(code2.length() - 1 - k));
+                    nbBitsPris++;
+                    if (nbBitsPris == nbBitsAPrendre) toutPris = true;
+                }
+
+            }
+            tabBits[i] = bits.toString();
+            //System.out.println("\n" + tabBits[i] + "\n");
+        }
+
+        //on lit les chaines et on addition chaque bit
+        int[] tabVal = new int[nbBitsDebug];
+        for (int i = tabBits.length - 1; i >= 0; i--)
+        {
+            int val = 0;
+            for (int j = 0; j < tabBits[i].length(); j++)
+            {
+                if (val == 0 && tabBits[i].charAt(j) == '1') val = 1;
+                else if (val == 1 && tabBits[i].charAt(j) == '1') val = 0;
+            }
+            tabVal[i] = val;
+            //System.out.println(val);
+        }
+
+        //System.out.println(code2);
+        str = new StringBuilder(code2);
+
+        //System.out.println(Arrays.toString(tabVal));
+
+
+        //placement des bits dans la chaine de retour
+        for (int k : tabVal)
+        {
+            char ch = Character.forDigit(k, 10);
+            //System.out.println(ch);
+            for (int j = 0; j < str.length(); j++)
+            {
+                if (str.charAt(j) == '_')
+                {
+                    str.setCharAt(j, ch);
+                    //System.out.println("C'est un " + str.charAt(i) + "au lieu d'un " + ch);
+                    break;
+                }
+            }
+        }
+
+        //on reinverse pour que le bit 1 soit a droite
+        code2 = new StringBuffer(str.reverse().toString());
+        //System.out.println(code2);
+
+        sRet += code2.toString();
+
+        return sRet;
+    }
+
+    public boolean isCodeCorrect(String code, boolean presenterEtapes)
+    {
+        int nbBitsDebug = 0;
+
+        for (int i = 0; Math.pow(2, i) <= code.length(); i++)
+            nbBitsDebug++;
+
+        String bits = String.format("%" + nbBitsDebug + "s", "").replaceAll(" ", "0");
+        // 0000 par exemple
+
+        boolean[] tabBitsCorrecteur = new boolean[nbBitsDebug]; // true = 0, false = 1
+        for (int i = 0; i < nbBitsDebug; i++)
+            tabBitsCorrecteur[i] = false;
+
+        StringBuilder tmp = new StringBuilder(bits);
+        for (int i = 0; i < nbBitsDebug; i++)
+        {
+            tmp.delete(0, tmp.length());
+            tmp.append(bits);
+
+            do
+            {
+                tmp.replace(nbBitsDebug - 1 - i, nbBitsDebug - i, "1");
+                int pos = Integer.parseInt(tmp.toString(), 2);
+
+                if (code.charAt(code.length() - pos) != '0') tabBitsCorrecteur[i] = !tabBitsCorrecteur[i];
+
+                tmp.delete(0, tmp.length());
+                tmp.append(Metier.valBitsSuivant(tmp.toString()));
+            } while (tmp.toString().contains("0"));
+
+            if (!tabBitsCorrecteur[i]) return false;
+        }
+
+        /*for (boolean bit : tabBitsCorrecteur)
+            if( !bit ) return false;
+        // pas opti
+        */
+
+        return true;
+    }
+
+    private static String valBitsSuivant(String toString)
+    {
+        if (!toString.contains("0")) return toString;
+
+        StringBuilder builder = new StringBuilder(toString);
+
+        boolean retenue;
+        int cpt = toString.length() - 1;
+
+        if (builder.charAt(cpt) == '0')
+        {
+            builder.replace(cpt, cpt + 1, "1");
+        }
+        else
+        {
+            builder.replace(cpt, cpt-- + 1, "0");
+            retenue = true;
+
+            while (retenue)
+            {
+                if (builder.charAt(cpt) == '0')
+                {
+                    builder.replace(cpt, cpt-- + 1, "1");
+                    retenue = false;
+                }
+                else
+                {
+                    builder.replace(cpt, cpt-- + 1, "0");
+                }
+            }
+        }
+
+        return builder.toString();
+    }
+
+    public static void main(String[] args)
+    {
+        Metier m = new Metier();
+        System.out.println(m.correctionPreEmission("1010"));
+        System.out.println(m.correctionPreEmission("1011"));
+        System.out.println(m.correctionPreEmission("10110111010"));
+        System.out.println(m.isCodeCorrect("1101101", false)); // doit etre faux
+        System.out.println(m.isCodeCorrect("101101111011011", false)); // doit etre vrai
+    }
 }
